161b029ab2ba9337a6ae48defab652e0275f6a4a
Pony <44317758+aspxcor@users.noreply.github.com>
2019-06-12 04:45:14 +0800

    完成程序主体结构

--- Project.Binary-Viewer/HexadecimalViewer.asm
+++ Project.Binary-Viewer/HexadecimalViewer.asm
@@ -10,10 +10,21 @@ data segment use16
 	controlLine db 9 dup('0')	;行输出控制的指示符
 	whereData dw 0 	;指示打印数据位置
 	whereAscii dw 0	;指示打印数据ASCII值位置
-	FirstDigital db 0 	;存储字符ASCII的高位数据
-	SecondDigital db 0 	;存储字符ASCII的低位数据
 data ends
 code segment use16
+switchKey:
+    mov ah,0
+	int 16h		;读取键盘信息
+	cmp ax,011Bh	;cmp with Esc
+	jz whilePressEsc
+	cmp ax,4700h	;cmp with Home
+	jz whilePressHome
+	cmp ax,4F00h	;cmp with End
+	jz whilePressEnd
+	cmp ax,4900h	;cmp with PageUp
+	jz whilePressPageUp
+	cmp ax,5100h	;cmp with PageDown
+	jz whilePressPageDown
 begin:
 	mov ax,0B800h                       
 	mov es,ax	;es与显卡关联
@@ -30,6 +41,7 @@ begin:
 	mov byte ptr[di],0
 	int 21h		;打开用户指定的文件
 	jnc Initialise
+exit:
 	mov ax, 4C00h
 	int 21h		;退出程序
 Initialise:		;此函数用于计算、存储文件信息并初始化窗口
@@ -104,6 +116,98 @@ loadChar:	;加载当前页数据的字符信息
 	xor ecx,ecx
 	mov edx,076h
 	mov whereData,dx
-	mov edx,20
+	mov edx,014h
 	mov whereAscii,dx
-printLine:		;对每行内容进行打印
+printLine:		;打印行号
+	cmp edx,014h	;对当前行是否输出完全进行判断，判断此函数是否应该被执行
+	jnz printChar
+	xor esi,esi
+	push di
+	lea di,controlLine
+charOfLineControl:	;对打印过程中是否为首行、字符位是否需要改变等细节进行控制，保证输出正确
+	cmp ecx,010h 	;关于是否为首行进行判断
+	mov ax,[di]
+	jc lineControlWhilePrinting	;第一行输出时要控制行参数加一
+	cmp esi,0Ch	;对字符进位的判断与控制
+	jnz lineControlWhilePrinting
+	inc eax		;对进位字符控制，将其自增1
+	cmp al,3Ah	;判断是否需要对数字转换为字母
+	jnz lineControlWhilePrinting
+	add eax,7
+lineControlWhilePrinting:	;在打印行号时进行必要格式控制与转换
+	mov [di],al
+	mov ah,07h
+	mov es:[ebx+esi],eax
+	add di,1
+	add esi,2	;加载行号信息
+	cmp esi,012h
+	jnz charOfLineControl	;若尚未完成加载则循环完成加载
+	pop di 		;解除保护，重新恢复di初始值
+printChar:		;打印字符它的十六进制形式ASCII码
+	mov si,whereData
+	mov ah,07h
+	mov al,[di]
+	mov es:[bx+si],ax
+	add esi,2
+	add ecx,1
+	mov whereData,si
+	mov si,whereAscii
+	mov ax,[di]
+	and ax,0FFh
+	mov dl,010h
+	div dl 	;读取字符ASCII码值
+	cmp al,0Ah
+	jnae isLower
+	add eax,07h
+isLower:	;对低位ASCII码取出讨论
+	add eax,030h
+	mov dh,al	;dh寄存器保存当前第一个ASCII字符数据
+	cmp ah,0Ah	
+	jnae isHigher
+	add eax,0700h
+isHigher:	;对高位ASCII码取出讨论
+	add eax,3000h	;将ah加'0'
+	push ax		;目的在于将低位ASCII码ah压入堆栈保护
+	mov al,dh 	;恢复先前al数值
+	mov ah,07h
+	mov es:[ebx+esi],eax
+	add esi,2	;实现对高位ASCII码显示
+	pop ax
+	mov al,ah
+	mov ah,07h 	;恢复ah值
+	mov es:[ebx+esi],eax
+	add esi,2	;实现对低位ASCII码显示
+	cmp esi,05Ah
+	jz setSeparator
+	cmp esi,042h
+	jz setSeparator
+	cmp esi,02Ah
+	jz setSeparator
+	mov al,' '
+	jmp short printSet
+setSeparator:	;设置打印分界线
+	mov al,'|'
+printSet:	;设置打印参数
+	mov es:[ebx+esi],eax
+	add esi,2
+	mov whereAscii,si
+	mov dx,whereData
+	add edi,1	;存储有关参数
+	cmp edx,150
+	jnz lineFeed	;换行输出
+	add ebx,160
+	mov edx,118
+	mov recordPosChar,dx
+	mov edx,20
+	mov recordPosAscii,dx 	;控制输出格式参数
+lineFeed:
+	cmp cx,dataIndex
+	jnz printLine
+	call switchKey
+whilePressEsc:	;关闭文件
+	mov ah, 3Eh
+	mov bx, filePointer
+	int 21h
+	call exit
+code ends
+end begin
